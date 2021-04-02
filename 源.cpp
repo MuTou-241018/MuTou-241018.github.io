5e24340b1d345be631db9bb582f90b548275bc2a
MuTou-241018 <80621032+MuTou-241018@users.noreply.github.com>
2021-04-02 05:56:03 -0500

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,21 +1,28 @@
-//ц╟ещеепР
+//иоб╔лщ
 #include<stdio.h>
-void main()
+#include<stdlib.h>
+int dpSeq(int n)
 {
-	int i, j, k=0;
-	int a[10];
-	for (i = 0; i < 10; i++)
-		scanf_s("%d", &a[i]);
-	for(i=0;i<10;i++)
-		for (j = 0; j < 10 - i; j++)
-		{
-			if (a[i] > a[i + 1])
-			{
-				k = a[i];
-				a[i] = a[i + 1];
-				a[i + 1] = k;
-			}
-		}
-	for (i = 0; i < 10; i++)
-		printf("%d\t", a[i]);
+	int* seq = (int*)malloc(sizeof(int) * n);
+	seq[0] = 1;
+	seq[1] = 2;
+	for (int i = 2; i < n; i++)
+		seq[i] = seq[i - 1] + seq[i - 2];
+	int mid = seq[n - 1];
+	free(seq);
+	return mid;
+}
+int stackSeq(int n)
+{
+	if (1 == n)
+		return 1;
+	if (2 == n)
+		return 2;
+	return stackSeq(n - 1) + stackSeq(n - 2);
+}
+int main(int argc, char const* argv[])
+{
+	printf("stack:%d\n", stackSeq(20));
+	printf("%DP:%d\n", dpSeq(20));
+	return 0;
 }
